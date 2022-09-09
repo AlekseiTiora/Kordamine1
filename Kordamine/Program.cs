681a86c20772811f4ae50948d58e1b53d9412105
opilane <opilane@HP-CZC6297B8F>
2022-09-09 08:57:49 +0300

    Add project files.

---
+++ Kordamine/Program.cs
@@ -0,0 +1,60 @@
+﻿using Kordamine;
+
+/*Koer koer = new Koer(Koer.toug.pudel, "Šarik", "punane", Koduloom.sugu.isane, 35.5, 7, true);
+koer.print_Info();
+koer.print_Haal();
+Koer veelkoer = new Koer(koer);
+veelkoer.muudaNimi("Pudel");
+veelkoer.muudaVarv("rohaline");
+veelkoer.muudaKaal(31);
+veelkoer.muudaVanus(5);
+veelkoer.print_Info();*/
+
+Papagoi papagoi_odin = new Papagoi(Papagoi.tougP.Ara, "Bobik", "must", Koduloom.sugu.emane, 2.1, 1, false);
+Papagoi papagoi_dva = new Papagoi(Papagoi.tougP.Jaco, "Samolet", "valge", Koduloom.sugu.emane, 3.1, 1, true);
+Papagoi papagoi_tri = new Papagoi(Papagoi.tougP.Kakaduu, "Vertalet", "kollane", Koduloom.sugu.emane, 4.1, 1, true);
+
+papagoi_odin.print_Info();
+papagoi_odin.print_Haal();
+Console.WriteLine(papagoi_odin.print_Info());
+
+List<Koduloom> Kooduloom = new List<Koduloom>();
+Kooduloom.Add(papagoi_odin);
+Kooduloom.Add(papagoi_dva);
+Kooduloom.Add(papagoi_tri);
+Console.WriteLine("\n");
+
+StreamWriter text = new StreamWriter(@"..\..\..\info.txt", false);
+foreach (Koduloom x in Kooduloom)
+{
+    text.WriteLine(x.nimi + "," + x.varv + "," + x.vanus);
+}
+text.Close();
+
+List<Papagoi> papagoi = new List<Papagoi>();
+StreamReader sw = new StreamReader(@"..\..\..\info.txt");
+string texts;
+while ((texts = sw.ReadLine()) != null)
+{
+    string[] rida = texts.Split(',');
+    papagoi.Add(new Papagoi(rida[0], rida[1]));
+}
+sw.Close();
+
+foreach (var item in papagoi)
+{
+    Console.WriteLine(item.nimi + " " + item.varv);
+}
+Console.ReadLine();
+
+
+
+/*string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "info.txt");
+using(StreamWriter sw = new StreamWriter(filePath))
+{
+    sw.WriteLine("--Koer--\n");
+    sw.WriteLine("{0} {1} {2} ta on {3} ja tema kaal on {4} ja ta on {5} aastat vana", koer.Toug,koer.nimi,koer.varv,koer.loomaSugu,koer.kaal,koer.vanus);
+
+    sw.WriteLine("\n--Papagoi--\n");
+    sw.WriteLine("{0} {1} {2} ta on {3} ja tema kaal on {4} ja ta on {5} aastat vana", papagoi.Toug, papagoi.nimi, papagoi.varv, papagoi.loomaSugu, papagoi.kaal, papagoi.vanus);
+}*/
---
+++ Kordamine/papagoi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine
+{
+    class Papagoi : Koduloom
+    {
+        public enum tougP { Jaco, Ara, Kakaduu, kakariki, kea };
+        public tougP TougPapagoi;
+
+        public Papagoi(tougP TougPapagoi, string nimi, string varv, sugu loomaSugu, double kaal, int vanus, bool elav) : base(nimi, varv, loomaSugu, kaal, vanus, elav)
+        {
+            this.TougPapagoi = TougPapagoi;
+        }
+
+        public Papagoi(string nimi, string varv)
+        {
+            this.nimi = nimi;
+            this.varv = varv;
+
+        }
+
+        public Papagoi(Papagoi papagoi) //kloonimiseks
+        {
+            this.nimi = papagoi.nimi;
+            this.TougPapagoi = papagoi.TougPapagoi;
+            this.varv = papagoi.varv;
+            this.loomaSugu = papagoi.loomaSugu;
+            this.kaal = papagoi.kaal;
+            this.vanus = papagoi.vanus;
+            this.elav = papagoi.elav;
+        }
+        
+        public Papagoi(string nimi)
+        {
+            this.nimi=nimi;
+        }
+
+        public override void print_Haal()
+        {
+            Console.WriteLine("kar kar");
+        }
+
+        public override string print_Info()
+        {
+            string text = $"Tõug on{TougPapagoi}, Tema nimi {nimi}, Värv on{varv}, papagoi sugu on{loomaSugu}, kaal on{kaal} ja tema vanus on{vanus}";
+
+            return text;
+        }
+        public void muudaNimi(string uusNimi) { nimi = uusNimi; }
+        public void muudaVarv(string Varv) { varv = Varv; }
+        public void muudaKaal(double Kaal) { kaal = Kaal; }
+        public void muudaVanus(int Vanus) { vanus = Vanus; }
+    }
+}
